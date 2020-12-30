25c4f0cee51352683dd0a5b1b67647442c6fce85
Juan Chavez <juan.chavezn1990@gmail.com>
2020-12-29 21:31:43 -0600

    Cambio de role controller a resource, creación de blade index para roles

---
+++ resources/views/Roles/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">
+                <div class="float-left">Lista de Roles</div>
+                <button class="btn btn-sm btn-primary float-right">Nuevo Rol</button>
+            </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="col-sm-8">Nombre</th>
+                                <th class="col-sm-4 justify-content-center">Opciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($roles as $role)
+                                <tr>
+                                    <td>{{ $role->Role_Name }}</td>
+                                    <td>
+                                        <button class="btn btn-sm btn-primary col-sm-3">Ver</button>
+                                        <button class="btn btn-sm btn-secondary col-sm-3">Editar</button>
+                                        <button class="btn btn-sm btn-danger col-sm-3" onclick="confirm('Seguro?')">Borrar</button>
+                                    </td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
